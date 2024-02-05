4d5f8420e7b7e792fbacb7902f01366885056027
virMavr <virtuozwork@gmail.com>
2024-02-05 15:24:56 +0300

    add smth new

---
+++ stack.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class StackArr {
+public:
+	StackArr() = default;
+	StackArr(const StackArr&) = default;
+	~StackArr() = default;
+	StackArr& operator=(const StackArr&) = default;
+
+
+private:
+
+
+};
+
+int main()
+{
+
+
+}
